246b5e0205058205dfb2835ffbade7d653c0f580
389390246hkz@sina.com <389390246hkz@sina.com>
2018-09-04 15:05:54 +0800

    删除getHkzMysqlList()

--- hnHousehold/src/main/java/com/startsi/hnhousehold/dao/HzDao.java
+++ hnHousehold/src/main/java/com/startsi/hnhousehold/dao/HzDao.java
@@ -3,9 +3,9 @@ package com.startsi.hnhousehold.dao;
 import java.util.List;
 
 import com.startsi.hnhousehold.model.Hkz;
-import com.startsi.hnhousehold.model.HkzMysql;
+
 
 public interface HzDao extends BaseDao{
 public List<Hkz> getHkzList() throws Exception;
-public List<HkzMysql> getHkzMysqlList() throws Exception;
+
 }
