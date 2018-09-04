fba85399adddfb3fe1a9200682db231290b4f13a
389390246hkz@sina.com <389390246hkz@sina.com>
2018-09-04 15:11:12 +0800

    增加逻辑

--- hnHousehold/src/main/java/com/startsi/hnhousehold/service/impl/HzServiceImpl.java
+++ hnHousehold/src/main/java/com/startsi/hnhousehold/service/impl/HzServiceImpl.java
@@ -16,7 +16,6 @@ public class HzServiceImpl implements HzService {
 	@Autowired
 	private HzDao hzDao;
 
-	
 	@Override
 	public void starthzService() throws Exception {
 		// TODO Auto-generated method stub
@@ -24,36 +23,38 @@ public class HzServiceImpl implements HzService {
 		System.out.println(hzDao);
 		System.out.println("进入service");
 		List<Hkz> hkzList = hzDao.getHkzList();
-		System.out.println("hkzList开始"+hkzList);
-		Hkz hkz=null;
-		if(hkzList!=null) {
-			 hkz = hkzList.get(0);
-		}
-		
-		System.out.println("查询开始");
-		try {
-		if (null!=hkz) {
-			System.err.println("有数据");
-			String id = hkz.getId();
-			String name = hkz.getName();
-			System.out.println(name);
-			HkzMysql mysql = new HkzMysql();
-			mysql.setId(id);
-			mysql.setName(name);
-			hzDao.saveObjectOfMysql(mysql);
-			hkz.setFlag("1");
-			hzDao.updateObject(hkz);
-			System.out.println("hkz更新成功" + hkz);
-			
-		} if((null==hkz)) {
-			System.out.println("没有数据");
-			} 
-		}catch (Exception e) {
-			// TODO: handle exception
-			hkz.setFlag("E");
-			hzDao.updateObject(hkz);
-			System.out.println("发生异常"+e.getMessage());
+		System.out.println("hkzList开始" + hkzList);
+		Hkz hkz = null;
+		if (hkzList != null) {
+			hkz = hkzList.get(0);
+
+			System.out.println("查询开始");
+			try {
+				if (null != hkz) {
+					System.err.println("有数据");
+					String id = hkz.getId();
+					String name = hkz.getName();
+					System.out.println(name);
+					HkzMysql mysql = new HkzMysql();
+					mysql.setId(id);
+					mysql.setName(name);
+					hzDao.saveObjectOfMysql(mysql);
+					hkz.setFlag("1");
+					hzDao.updateObject(hkz);
+					System.out.println("hkz更新成功" + hkz);
+
+				}
+				if ((null == hkz)) {
+					System.out.println("没有数据");
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+				hkz.setFlag("E");
+				hzDao.updateObject(hkz);
+				System.out.println("发生异常" + e.getMessage());
+			}
+		}else {
+			System.out.println("对不起,不存在要传输的对象");
 		}
 	}
-
 }
